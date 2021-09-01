63fd383edcc864ffb7f56b4f1bba07baf7985568
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2021-09-01 19:36:35 -0400

    Criando methds get -post

--- api/routes/standup.js
+++ api/routes/standup.js
@@ -3,6 +3,27 @@ const Standup = require("../../models/standup")
 module.exports = function (router) {
     //GET: the 12 newest stand-up meeting notes
     router.get('/standup', function (req, res) {
+        Standup.find()
+            .sort({ 'createdOn': -1 })
+            .exec()
+            .then(docs => res.status(200).json(docs))
+            .catch(err => res.status(500).json({
+                message: 'Error finding standup meeting notes',
+                error: err
+            }))
+
+    })
+    
+    //GET by id
+    router.get('/standup/:id', function (req, res) {
+        Standup.findById(req.params.id)
+            .sort({ 'createdOn': -1 })
+            .exec()
+            .then(docs => res.status(200).json(docs))
+            .catch(err => res.status(500).json({
+                message: 'Error finding standup meeting notes',
+                error: err
+            }))
 
     })
 
